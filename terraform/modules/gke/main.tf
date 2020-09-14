61b46603f0e1581520ba4cb3f91510534036ec2b
Micah <mmiller18@slb.com>
2020-09-13 22:29:31 -0600

    debug

--- terraform/modules/gke/main.tf
+++ terraform/modules/gke/main.tf
@@ -155,17 +155,13 @@ provider "kubernetes" {
 #     }
 # }
 
-# provider "helm" {
-#   kubernetes {
-#     host     = google_container_cluster.primary.endpoint
-#     username = "ClusterMaster"
-#     password = "MindTheGap"
-
-#     client_certificate     = google_container_cluster.primary.master_auth.0.client_certificate
-#     client_key             = google_container_cluster.primary.master_auth.0.client_key
-#     cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
-#   }
-# }
+provider "helm" {
+  kubernetes {
+    host     = google_container_cluster.primary.endpoint
+    token    = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
+  }
+}
 
 resource "helm_release" "goserver" {
     # depends_on = [null_resource.kubectl]
